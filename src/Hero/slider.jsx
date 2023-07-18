5c8cd86a26eb563d57f0e6e03e9a6aad3851127f
Ohzuma <ozumanology@gmail.com>
2023-07-18 00:26:22 -1200

    hero-section done

--- src/Hero/slider.jsx
+++ src/Hero/slider.jsx
@@ -1,12 +1,34 @@
-// import 
-function Slider (){
-    return(
-        <div className="silder">
-            hhh
-        <div>peter</div>
-            <p>saghjk;hug</p>
-       </div>
-    )
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay } from "swiper/modules";
+import "swiper/css/bundle";
+import SwiperCore from "swiper";
+// import {SiReactrouter,SiSwiper} from react-icons/si
+// import { FaReact } from react - icons / fa
+
+
+
+function Slider() {
+  SwiperCore.use([Autoplay]);
+  return (
+    <div className="slider">
+      <header>
+        <h1>Technologies Used</h1>
+      </header>
+          <div className="mySwiper">
+                 <Swiper
+        spaceBetween={50}
+        slidesPerView={3}
+        autoplay={{ delay: 1000 }}
+        loop={true}
+      >
+        <SwiperSlide>1</SwiperSlide>
+        <SwiperSlide>2</SwiperSlide>
+        <SwiperSlide>3</SwiperSlide>
+        <SwiperSlide>4</SwiperSlide>
+      </Swiper>
+   </div>
+    </div>
+  );
 }
 
-export default Slider
+export default Slider;
